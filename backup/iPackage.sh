793ada225eb0d4d09a655b47363492c731351e52
leeimgyu <dldlarb456@naver.com>
2025-09-18 11:39:24 +0900

    0918

--- backup/iPackage.sh
+++ backup/iPackage.sh
@@ -8,6 +8,12 @@ npm i chance luxon @fontsource/material-icons
 echo "Installing TypeScript dependencies..."
 npm i -D @types/chance @types/luxon
 
+echo "Installing Development dependencies..."
+npm i -D postcss autoprefixer tailwindcss
+npm install tailwindcss @tailwindcss/vite
+npm install tailwindcss @tailwindcss/postcss postcss
+npm install tailwindcss @tailwindcss/cli
+
 echo "Copy Files..."
 cp C:/workspace/spaceReact/20250911_react/backup/.*.cjs .
 cp C:/workspace/spaceReact/20250911_react/backup/index.html .
--- ch03/ch03_2/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const { plugin } = require("postcss");
-
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
